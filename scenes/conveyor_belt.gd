c76ae8e4f59ecd50bd95d331eed350883315b657
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2024-11-23 12:07:53 -0800

    began conveyor

---
+++ scenes/conveyor_belt.gd
@@ -0,0 +1,10 @@
+class_name ConveyorBelt extends StaticBody2D
+
+@export var speed: float = 200
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
