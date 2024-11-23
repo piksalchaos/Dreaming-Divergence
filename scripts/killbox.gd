931f5c5b601f1735a53fa1e102191b68b6222872
Wes Le <88017350+basedIdiot@users.noreply.github.com>
2024-11-23 15:08:09 -0800

    new spike texture

--- scripts/killbox.gd
+++ scripts/killbox.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
-@export var is_enabled: bool = false
+@export var is_enabled: bool
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	update_killbox_state()
+func update_killbox_state():
+	if is_enabled:
+		$Sprite2D.frame = 0
+	else:
+		$Sprite2D.frame = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_body_entered(body: Node2D):
@@ -15,6 +19,7 @@ func _on_body_entered(body: Node2D):
 
 func switch():
 	is_enabled = not is_enabled
+	update_killbox_state()
 	if not is_enabled:
 		return
 	for body in get_overlapping_bodies():
