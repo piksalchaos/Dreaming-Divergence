0532289ac4177647c8acd71d9b2bbf51aa1756ae
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2024-11-23 13:35:03 -0800

    conveyor belt works!

--- scenes/conveyor_belt.gd
+++ scenes/conveyor_belt.gd
@@ -1,10 +1,21 @@
 class_name ConveyorBelt extends StaticBody2D
 
-@export var speed: float = 200
+@export var speed: float = 50
+@export var is_direction_right = true
+@export var is_enabled = true
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
 func _ready() -> void:
-	pass # Replace with function body.
+	sprite_2d.flip_h = not is_direction_right
 
+func get_velocity():
+	if not is_enabled:
+		return 0
+	if is_direction_right:
+		return speed
+	else:
+		return -speed
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func switch():
+	is_enabled = not is_enabled
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,31 +1,31 @@
-class_name Player
+class_name Player extends CharacterBody2D
 
-extends CharacterBody2D
-
+signal killed
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-signal killed
+@onready var floor_checker: Area2D = $FloorChecker
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	for body in floor_checker.get_overlapping_bodies():
+		if body is ConveyorBelt:
+			velocity.x += body.get_velocity()
+	
 	move_and_slide()
+
 func kill():
 	killed.emit()
 	queue_free()
