93919ff62a0aaa28273807658be969f8cc1110a2
Wes Le <88017350+basedIdiot@users.noreply.github.com>
2024-11-23 11:26:50 -0800

    change main scene and reorganize

--- scripts/killbox.gd
+++ scripts/killbox.gd
@@ -8,6 +8,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_body_entered(body: PhysicsBody2D):
-	print("boo")
 	if body.is_in_group("player"):
 		body.kill()
