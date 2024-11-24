33d81e16bc70d4f6eb30b783f91a05c12cb36229
Wes Le <88017350+basedIdiot@users.noreply.github.com>
2024-11-24 14:49:19 -0800

    fuck

--- scripts/player.gd
+++ scripts/player.gd
@@ -28,8 +28,16 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.play()
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.animation = "idle"
+	
+	if direction < 0:
+		$AnimatedSprite2D.flip_h = true
+	elif direction > 0:
+		$AnimatedSprite2D.flip_h = false
 
 	move_and_slide()
 func kill():
